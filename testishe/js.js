cc6cfeae0d37700d030caf42e75a24709acc5525
hackerniy <59176703+hackerniy@users.noreply.github.com>
2020-02-12 20:06:51 +0300

    opa

--- testishe/js.js
+++ testishe/js.js
@@ -20,6 +20,7 @@ function hi(name){
 
 let quest = ["HTML - язык програмирования?", "Css это не язык программирования?", "Сколько базовых тегов в HTML", "Сколько тегов html в HTML коде может быть?", "У тега input есть закрывающий тег?", "А в теге <b>?", "Запустится ли код, если не закрыть тег <html>?", "Существует ли тег <H1>?", "Может ли id, начинающийся с цифры, выполнять те же функции, что и id, начинающийся с буквы?", "Может ли высота HTML файла быть бесконечной?"]
 let answ = ["нет", "да", "4", "2", "нет", "да", "да", "да", "нет", "да"]
+let leng = quest.length[]
 
 function test(){
 	$('body').empty(); 
@@ -29,21 +30,7 @@ function test(){
 	$('h1').text(quest[0])
 }
 
-function test1(){ 
-
-	let score = 0;
-	for( let i = 0; i < quest.length; i++ ){
-		let q = quest[i];
-		let a = answ[i];
-		let otvet = prompt("Вопрос № " + (i+1) + ": \n" + q );
-		if (otvet == a){
-			score = score + 1
-		}
-	}
-	alert(name + ", ты набрал " + score + " / " + answ.length + " правильных вопросов")
-}
-
-function check{
+function check() {
 	let otv = $('input').val().toLowerCase();
 
 	if (otvet == answ){
@@ -58,25 +45,25 @@ function check{
 	}
 	else{
 		theEnd();
-	}
-	
+	}	
 }
 
 function theEnd(){
 	$('body').empty();
-	$('body').append('<p class="pf">Итак, '+name+', ты ответил на '+score+' из '+answ+'</p>')
+	$('body').append('<p class="pf">Итак, '+name+', ты ответил на '+score+' из '+leng+'</p>')
 }
 
 function start(){
 	//Удаляем элемент с id="start"
 	$('#start').remove()
+	$('body').append('<div id="div"></div>')
+	$('#div').css()
 	//Создаем абзац с текстом
-	$('body').append('<p>Введите свое имя...</p>')
+	$('#div').append('<p>Введите свое имя...</p>')
 	//Создаем поле для ввода
-	$('body').append('<input type="text" id="name">')
+	$('#div').append('<input type="text" id="name">')
 	//Создаем кнопку для отправки
-	$('body').append('<input type="submit" id="send">')
+	$('#div').append('<input type="submit" id="send">')
 	//Задаём атрибуту onclick название функции
 	$('#send').attr('onclick', 'getName()');
-	
 }	
